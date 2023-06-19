1d13226edd4e93e44dbd4d1d38f1d99e35fb8694
Grannik <81516234+Grannik@users.noreply.github.com>
2023-06-19 23:16:38 +0200

    Add files via upload

---
+++ GrnAudio/GrnAudio.js
@@ -0,0 +1,8 @@
+function farchive(){window.open('https://archive.org/details/@grannik','_blank');}
+function fspotifyRound(){window.open('https://open.spotify.com/user/dqorp6bhteoq7qx9xaaqqc8fv','_blank');}
+function fsoundcloud(){window.open('https://soundcloud.com/user-669870194','_blank');}
+function fvoxworker(){window.open('https://voxworker.com/ru','_blank');}
+function fBandcamp(){window.open('https://bandcamp.com/grannikoleg','_blank');}
+function fdiscogs(){window.open('https://www.discogs.com/ru/user/Grannik','_blank');}
+function ftelegram(){window.open('https://web.telegram.org/','_blank');}
+function fwhatsapp(){window.open('https://web.whatsapp.com/','_blank');}
