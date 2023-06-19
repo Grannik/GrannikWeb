54218655857005f12c43b1caa179707a5cf3881e
Grannik <81516234+Grannik@users.noreply.github.com>
2023-06-19 23:28:14 +0200

    Add files via upload

---
+++ GrnFile/GrnFile.js
@@ -0,0 +1,26 @@
+function fcryptgeon(){window.open('https://cryptgeon.nicco.io/','_blank');}
+function fseatable(){window.open('https://cloud.seatable.io/dtable/','_blank');}
+function fElement(){window.open('https://app.element.io/#/welcome','_blank');}
+function fAnonfiles(){window.open('https://anonfiles.com/','_blank');}
+function fCryptpad(){window.open('https://cryptpad.org/','_blank');}
+function ffileio(){window.open('https://www.file.io/','_blank');}
+function fWetransfer(){window.open('https://wetransfer.com/','_blank');}
+function fJumbomail(){window.open('https://www.jumbomail.me/ru/','_blank');}
+function fDropmefiles(){window.open('https://dropmefiles.com/','_blank');}
+function fFilecloud(){window.open('https://filecloud.me/','_blank');}
+// --- Расшарить файл --------------------------------------------------
+function fPixeldrain(){window.open('https://pixeldrain.com/','_blank');}
+function fSendAnywhere(){window.open('https://send-anywhere.com/','_blank');}
+function fEvecloud(){window.open('https://auth.evecloud.xyz/auth/login','_blank');}
+function fTerashare(){window.open('http://terashare.net/','_blank');}
+function fEgnyte(){window.open('https://www.egnyte.com/','_blank');}
+function fTresorit(){window.open('https://tresorit.com/','_blank');}
+function fFex(){window.open('https://fex.net/','_blank');}
+function fFilemail(){window.open('https://www.filemail.com/','_blank');}
+function fTransferxl(){window.open('https://www.transferxl.com/','_blank');}
+function fMailbigfil(){window.open('https://free.mailbigfile.com/','_blank');}
+function fSendthisfile(){window.open('https://www.sendthisfile.com/','_blank');}
+function fFiledropper(){window.open('https://www.filedropper.com/','_blank');}
+function fToffeeshare(){window.open('https://toffeeshare.com/','_blank');}
+// --- Псомотреть позже ----------------------------------------------
+function fWikisend(){window.open('https://wikisend.com/','_blank');}
