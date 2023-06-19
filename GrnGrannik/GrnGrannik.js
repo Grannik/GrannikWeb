1b67b9f946109fa8e21776cb2d1dc56565b19a79
Grannik <81516234+Grannik@users.noreply.github.com>
2023-06-19 23:35:13 +0200

    Add files via upload

---
+++ GrnGrannik/GrnGrannik.js
@@ -0,0 +1,6 @@
+function fnetlify(){window.open('https://app.netlify.com/','_blank');}
+function fnetlifyGrn(){window.open('https://transcendent-snickerdoodle-ac246c.netlify.app/','_blank');}
+function fneocities(){window.open('https://neocities.org/','_blank');}
+function fneocitiesGrn(){window.open('https://grannik.neocities.org/','_blank');}
+function fgithub(){window.open('https://github.com','_blank');}
+function fgithubGrn(){window.open('https://grannik.github.io/Grannikremain.github.io/','_blank');}
