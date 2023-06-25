a9a4705276555253157c4b6ba42e2c24fd7fbf9b
Grannik <81516234+Grannik@users.noreply.github.com>
2023-06-25 22:15:18 +0200

    Delete GrnGrannik.js

--- GrnGrannik/GrnGrannik.js
+++
@@ -1,6 +0,0 @@
-function fnetlify(){window.open('https://app.netlify.com/','_blank');}
-function fnetlifyGrn(){window.open('https://transcendent-snickerdoodle-ac246c.netlify.app/','_blank');}
-function fneocities(){window.open('https://neocities.org/','_blank');}
-function fneocitiesGrn(){window.open('https://grannik.neocities.org/','_blank');}
-function fgithub(){window.open('https://github.com','_blank');}
-function fgithubGrn(){window.open('https://grannik.github.io/GrannikWeb.github.io/','_blank');}
