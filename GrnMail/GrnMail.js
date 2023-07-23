b71277977d77f86fb249668e57e70a7e85f91c34
Grannik <81516234+Grannik@users.noreply.github.com>
2023-07-23 22:03:29 +0200

    Add files via upload

---
+++ GrnMail/GrnMail.js
@@ -0,0 +1,18 @@
+function fprotonmail(){window.open('https://mail.protonmail.com/','_blank');}
+function fgmail(){window.open('https://mail.google.com/mail','_blank');}
+function fmeta(){window.open('https://webmail.meta.ua/','_blank');}
+function ftutanota(){window.open('https://mail.tutanota.com/','_blank');}
+function foutlook(){window.open('https://outlook.live.com/','_blank');}
+function fmailfence(){window.open('https://www.mailfence.com/','_blank');}
+// Одноразовые почтовые ящики
+function fyopmail(){window.open('http://www.yopmail.com/en','_blank');}
+function fgetnada(){window.open('https://getnada.com/','_blank');}
+function fmoakt(){window.open('https://www.moakt.com','_blank');}
+function ftempail(){window.open('https://tempail.com/ru/','_blank');}
+function femailondeck(){window.open('https://www.emailondeck.com','_blank');}
+function fanonbox(){window.open('https://anonbox.net','_blank');}
+function fguerrillamail(){window.open('https://www.guerrillamail.com/inbox','_blank');}
+function ftempmail(){window.open('https://temp-mail.org/','_blank');}
+function f10minutemail(){window.open('https://10minutemail.com/','_blank');}
+function fmailnesia(){window.open('http://mailnesia.com/','_blank');}
+function fPlustransfer(){window.open('https://www.plustransfer.com/','_blank');}
