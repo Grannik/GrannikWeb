62dde3a7522f7f753160d8a1d881ad69fe76e5cb
Grannik <81516234+Grannik@users.noreply.github.com>
2023-07-31 02:22:44 +0200

    Add files via upload

---
+++ GrnFile/GrnFile.js
@@ -0,0 +1,25 @@
+//
+function fcryptgeon(){window.open('https://cryptgeon.nicco.io/','_blank');}
+function fseatable(){window.open('https://cloud.seatable.io/dtable/','_blank');}
+function fElement(){window.open('https://app.element.io/#/welcome','_blank');}
+function fAnonfiles(){window.open('https://anonfiles.com/','_blank');}
+function fCryptpad(){window.open('https://cryptpad.org/','_blank');}
+function ffileio(){window.open('https://www.file.io/','_blank');}
+function fWetransfer(){window.open('https://wetransfer.com/','_blank');}
+function fJumbomail(){window.open('https://www.jumbomail.me/ru/','_blank');}
+function fDropmefiles(){window.open('https://dropmefiles.com/','_blank');}
+function fFilecloud(){window.open('https://filecloud.me/','_blank');}
+function fPeergos(){window.open('https://peergos.net/','_blank');}
+function ftransfer(){window.open('https://transfer.sh/','_blank');}
+function fToffeeshare(){window.open('https://toffeeshare.com/','_blank');}
+//
+function fcatbox(){window.open('https://catbox.moe/','_blank');}
+function f0x0(){window.open('https://0x0.st/','_blank');}
+function fPixeldrain(){window.open('https://pixeldrain.com/','_blank');}
+function fFex(){window.open('https://fex.net/','_blank');}
+// ---------------------------------------------------------------------
+// --- Расшарить файл --------------------------------------------------
+function fFilemail(){window.open('https://www.filemail.com/','_blank');}
+function fTransferxl(){window.open('https://www.transferxl.com/','_blank');}
+function fSendthisfile(){window.open('https://www.sendthisfile.com/','_blank');}
+function fWikisend(){window.open('https://wikisend.com/','_blank');}
