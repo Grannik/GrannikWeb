69ebddb51d0c7e47e99e92f8583de92191811858
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 12:58:19 +0200

    Add files via upload

---
+++ GrnCSS/GrnCSS.js
@@ -0,0 +1,20 @@
+function fhtml5css(){window.open('https://html5css.ru/cssref/default.php','_blank');}
+function fhtml5(){window.open('https://html5css.ru/tags/default.php','_blank');}
+function fq112css()
+{
+ var display = document.getElementById('iq112css').style.display;
+   if (display == 'none') {document.getElementById('iq112css').style.display = 'block';}
+ else                     {document.getElementById('iq112css').style.display = 'none'; }
+}
+function fq112cssR()
+{
+ var display = document.getElementById('iq112cssCcl').style.display;
+   if (display == 'none') {document.getElementById('iq112cssCcl').style.display = 'block';}
+ else                     {document.getElementById('iq112cssCcl').style.display = 'none'; }
+}
+function fq112cssD()
+{
+ var display = document.getElementById('iq112cssD').style.display;
+   if (display == 'none') {document.getElementById('iq112cssD').style.display = 'block';}
+ else                     {document.getElementById('iq112cssD').style.display = 'none'; }
+}
