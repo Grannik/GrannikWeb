704d4e95d33f24e3771483e934f96da93429d609
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 13:26:31 +0200

    Add files via upload

---
+++ GrnGifSvg/GrnGifSvg.js
@@ -0,0 +1,63 @@
+function fphotomosh(){window.open('https://photomosh.com/','_blank');}
+function fgiphy(){window.open('https://giphy.com/gifs/bbEpAuSQah7O90JM9f','_blank');}
+function ftinypic(){window.open('https://tinypic.host/grannik/albums','_blank');}
+
+function fSpStGifA()
+{
+ var display = document.getElementById('iSpStGifA').style.display;
+      if (display == 'none') {
+        document.getElementById('iSpStGifA').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('iSpStGifA').style.display = 'none'; // Скрыть блок
+      }
+    }
+
+function fSpStGifB()
+{
+ var display = document.getElementById('iSpStGifB').style.display;
+      if (display == 'none') {
+        document.getElementById('iSpStGifB').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('iSpStGifB').style.display = 'none'; // Скрыть блок
+      }
+}
+
+function f112qA()
+{
+ var display = document.getElementById('i112qA').style.display;
+      if (display == 'none') {
+        document.getElementById('i112qA').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('i112qA').style.display = 'none'; // Скрыть блок
+      }
+}
+
+function f112qB()
+{
+ var display = document.getElementById('i112qB').style.display;
+      if (display == 'none') {
+        document.getElementById('i112qB').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('i112qB').style.display = 'none'; // Скрыть блок
+      }
+}
+
+function f112qC()
+{
+ var display = document.getElementById('i112qC').style.display;
+      if (display == 'none') {
+        document.getElementById('i112qC').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('i112qC').style.display = 'none'; // Скрыть блок
+      }
+}
+
+function f112qD()
+{
+ var display = document.getElementById('i112qD').style.display;
+      if (display == 'none') {
+        document.getElementById('i112qD').style.display = 'block'; // Показать блок
+      } else {
+        document.getElementById('i112qD').style.display = 'none'; // Скрыть блок
+      }
+}
