74761036ee8d60bf898111501afd79d9e7eab278
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 14:34:50 +0200

    Add files via upload

---
+++ GrnGrannik/GrWeapons/GrWeapons.js
@@ -0,0 +1,16 @@
+
+// Функция для отображения элемента
+ function overmsg()
+{
+ var element = document.getElementById("imsg");
+ element.style.display = "block";
+}
+
+//
+ function eoutmsg()
+{
+ var element = document.getElementById("imsg");
+ element.style.display = "none";
+}
+
+function fweapons(){window.open('../GrWeapons/GrWeapons.html','_blank');}
