c2c15f4a5b331748df7a38d6b78e96115109235a
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 15:15:56 +0200

    Add files via upload

---
+++ GrnPctVid/GrnPctVid.js
@@ -0,0 +1,44 @@
+function farchive(){window.open('https://archive.org/details/@grannik','_blank');}
+function fmewe(){window.open('https://mewe.com/i/olegzhdanov','_blank');}
+function fpatreon(){window.open('https://www.patreon.com/user?u=49968756','_blank');}
+function fFlickr(){window.open('https://www.flickr.com/photos/197316470@N06/with/52594719544/','_blank');}
+function finstagram(){window.open('https://www.instagram.com/zhdanov8033/','_blank');}
+function fpinterest(){window.open('https://www.pinterest.at/olegzhdanovgrannik/_saved/','_blank');}
+function flivejournal(){window.open('https://www.livejournal.com/post/list?tab=posted','_blank');}
+function ftumblr(){window.open('https://www.tumblr.com/blog/grannik','_blank');}
+// ----- Функция для *** Blogger *** --------------------------------------------------------------------------------------
+var clickIle = 0;
+// Выполнить функцию при одинарном клике
+function fIle() { clickIle++; setTimeout(function() { if (clickIle === 1) { fBlogger(); } clickIle = 0; }, 250);}
+// Выполнить функцию при двойном клике
+function ffIle() {ffBlogger();}
+function fBlogger(){window.open('https://grannikarticle.blogspot.com/','_blank');}
+function ffBlogger(){window.open('https://www.blogger.com/blog/posts/6880022931953463852','_blank');}
+//---------------------------------------------------------------------------------------------------------------------------
+function ffacebook(){window.open('https://www.facebook.com/profile.php?id=100009295709012','_blank');}
+function fusalife(){window.open('https://usa.life/Grannik','_blank');}
+function flinkedin(){window.open('https://www.linkedin.com/in/oleg-zhdanov-b503a5226/','_blank');}
+function fgab(){window.open('https://gab.com/Grannik','_blank');}
+// ----- Функция для *** pixelfed *** --------------------------------------------------------------------------------------
+var clickIlb = 0;
+// Выполнить функцию при одинарном клике
+function singleIlb() { clickIlb++; setTimeout(function() { if (clickIlb === 1) { fpixelfed(); } clickIlb = 0; }, 250);}
+// Выполнить функцию при двойном клике
+function doubleIlb() {ffpixelfed();}
+function fpixelfed(){window.open('https://portfolio.pixelfed.social/Grannik','_blank');}
+function ffpixelfed(){window.open('https://pixelfed.social/i/web/profile/332238422003552256','_blank');}
+//---------------------------------------------------------------------------------------------------------------------------
+function fminds(){window.open('https://www.minds.com/grannik/','_blank');}
+function ftelegram(){window.open('https://web.telegram.org/','_blank');}
+function fwhatsapp(){window.open('https://web.whatsapp.com/','_blank');}
+function fimgur(){window.open('https://imgur.com/user/Grannik/posts','_blank');}
+function fopenvk(){window.open('https://openvk.su/id10111','_blank');}
+function ftwitter(){window.open('https://twitter.com/Grannik2','_blank');}
+function fgettr(){window.open('https://gettr.com/user/grannikol','_blank');}
+function fmastodont(){window.open('https://mas.to/@Granniks','_blank');}
+function fdribbble(){window.open('https://dribbble.com/Grannik/shots','_blank');}
+function fSlack(){window.open('https://app.slack.com/','_blank');}
+function fpixabay(){window.open('https://pixabay.com/ru/accounts/media/','_blank');}
+function fpexels(){window.open('https://www.pexels.com/@zhdanov-oleg-144981584','_blank');}
+function freddit(){window.open('https://www.reddit.com/user/Ok-Passion1783/submitted/','_blank');}
+function fmediaio(){window.open('https://www.media.io/','_blank');}
