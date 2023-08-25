a5d14e1858b260de87440b007b10af19515e8096
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 15:26:08 +0200

    Add files via upload

---
+++ GrnText/GrnText.js
@@ -0,0 +1,32 @@
+function fprivnote(){window.open('https://privnote.com/#','_blank');}
+function fonetimesecret(){window.open('https://onetimesecret.com//','_blank');}
+function fprivatebin(){window.open('https://privatebin.support-tools.com/','_blank');}
+function fgnusocial(){window.open('https://gnusocial.network/try/','_blank');}
+function fmemo(){window.open('https://memo.cash/','_blank');}
+function fmanyver(){window.open('https://www.manyver.se/','_blank');}
+function fSpeekNetwork(){window.open('https://speek.network/','_blank');}
+// --- Переводчики языка ---------------------------------------------------
+function fDeepl(){window.open('https://www.deepl.com/translator','_blank');}
+function flibretranslate(){window.open('https://libretranslate.com/','_blank');}
+function flingva(){window.open('https://lingva.ml/auto/en/ru','_blank');}
+function fgoogletranslate(){window.open('https://translate.google.com/','_blank');}
+function flanguagetool(){window.open('https://languagetool.org/ru/','_blank');}
+// --- Текстовый хостинги --------------------------------------------------
+function fpastebin(){window.open('https://pastebin.com/','_blank');}
+function fPastenym(){window.open('https://pastenym.ch/#/','_blank');}
+function fpasteburn(){window.open('https://pasteburn.onrender.com/','_blank');}
+function fFriendpaste(){window.open('https://friendpaste.com/','_blank');}
+function fpastie(){window.open('http://pastie.org/','_blank');}
+function fJustnotepad(){window.open('http://justnotepad.com/ru/','_blank');}
+function fNotesio(){window.open('https://notes.io/','_blank');}
+function fShrib(){window.open('https://shrib.com/','_blank');}
+function fPinup(){window.open('https://pinup.com/','_blank');}
+function fAnotepad(){window.open('https://anotepad.com/','_blank');}
+function fZippyjot(){window.open('https://www.zippyjot.com/','_blank');}
+function fItextpad(){window.open('https://www.itextpad.com/','_blank');}
+function fTelegraph(){window.open('https://telegra.ph/','_blank');}
+function fwtsocial(){window.open('https://wt.social/u/grannik','_blank');}
+// Конвертируйте ODT в DOCX документы онлайн
+function fKonverterOdtVdocx(){window.open('https://anyconv.com/ru/konverter-odt-v-docx/','_blank');}
+function fopenfile(){window.open('https://open-file.ru/','_blank');}
+function fImageToTextOCR(){window.open('https://image-to-text-ocr.netlify.app/','_blank');}
