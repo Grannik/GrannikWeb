8ae0bccc165810cea8d28a5ff3a154e8304037d8
Grannik <81516234+Grannik@users.noreply.github.com>
2023-08-25 15:21:36 +0200

    Add files via upload

---
+++ GrnPictur/GrnPictur.js
@@ -0,0 +1,38 @@
+// ----- Функция для *** Taplink *** ---------------------------------------------------------------------------------------
+var clickIla = 0;
+// Выполнить функцию при одинарном клике
+function singleIla() { clickIla++; setTimeout(function() { if (clickIla === 1) {fTaplink(); } clickIla = 0; }, 250);}
+// Выполнить функцию при двойном клике
+function doubleIla() {ffTaplink();}
+function fTaplink(){window.open('https://taplink.cc/grannik','_blank');}
+function ffTaplink(){window.open('https://taplink.ru/profile/13065605/pages/','_blank');}
+//---------------------------------------------------------------------------------------------------------------------------
+function fFlickr(){window.open('https://www.flickr.com/photos/197316470@N06/with/52594719544/','_blank');}
+//---------------------------------------------------------------------------------------------------------------------------
+function fUnsplash(){window.open('https://unsplash.com/@granikc','_blank');}
+function fartstation(){window.open('https://www.artstation.com/olegzhdanovgrannik/collections','_blank');}
+function fsteamcommunity(){window.open('https://steamcommunity.com/profiles/76561199245060110/','_blank');}
+function fdeviantart(){window.open('https://www.deviantart.com/grannik/gallery','_blank');}
+function fdiscord(){window.open('https://discord.com/channels/808391689113829408/808391690179051593','_blank');}
+function fbuymeacoffee(){window.open('https://www.buymeacoffee.com/Grannik/posts','_blank');}
+function fkofi(){window.open('https://ko-fi.com/grannik76723#paypalModal','_blank');}
+function fImgbb(){window.open('https://oleg-zhdanov-gra.imgbb.com/','_blank');}
+function fPhotosGoogle(){window.open('https://photos.google.com/','_blank');}
+function fpickupimage(){window.open('https://pickupimage.com/auth/myphotos.cfm?CFID=8f086144-9b16-4492-b259-c09ce13a3b85&CFTOKEN=0','_blank');}
+function fDeepdreamgenerator(){window.open('https://deepdreamgenerator.com','_blank');}
+function ffriendica(){window.open('https://libranet.de/profile/grannik','_blank');}
+function fbastyon(){window.open('https://bastyon.com/index','_blank');}
+function f500px(){window.open('https://500px.com/p/grannik?view=photos','_blank');}
+function fPiwigo(){window.open('https://grannik.piwigo.com/','_blank');}
+function fPostimage(){window.open('https://postimg.cc/files','_blank');}
+function fGravatar(){window.open('https://en.gravatar.com/grannikoleg','_blank');}
+function fdiaspora(){window.open('https://diaspora.freifunk-naila.net/people/042496d0e398013b6cc4525400123419','_blank');}
+function fwattpad(){window.open('https://www.wattpad.com/','_blank');}
+function ffreetalk(){window.open('https://freetalk.app/Grannik','_blank');}
+function fwikimedia(){window.open('https://commons.wikimedia.org/wiki/User:Grannik','_blank');}
+function fpetalica(){window.open('https://petalica.com/index_en.html','_blank');}
+function fprismaai(){window.open('https://app.prisma-ai.com/','_blank');}
+function fLunapic(){window.open('https://www8.lunapic.com/editor/','_blank');}
+function fimglarger(){window.open('https://imglarger.com/','_blank');}
+function fimageupscaler(){window.open('https://imageupscaler.com/','_blank');}
+function ftinypic(){window.open('https://tinypic.host/grannik/albums','_blank');}
